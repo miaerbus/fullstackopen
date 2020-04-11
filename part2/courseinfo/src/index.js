a18055d0c53c4ce08aad9127e96d93288b14ba49
Mia Erbus <mia.erbus@gmail.com>
2020-04-11 15:29:40 +0200

    [part2] course contents step6

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+
+
+// const Total = (props) => {
+//   return (
+//     <div>
+//       <p>
+//         Number of exercises{' '}
+//         {props.parts[0].exercises +
+//           props.parts[1].exercises +
+//           props.parts[2].exercises}
+//       </p>
+//     </div>
+//   )
+// }
+
+ReactDOM.render(<App />, document.getElementById('root'))
