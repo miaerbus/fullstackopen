c4c4814cfecc35ae6c11d62698f66acad9ec7225
Mia Erbus <mia.erbus@gmail.com>
2020-04-13 13:09:14 +0200

    [part2] notes db axios

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note...')
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios.get('http://localhost:3001/notes').then((response) => {
+      console.log('promise resolved')
+      setNotes(response.data)
+    })
+  }
+  useEffect(hook, [])
+  console.log('render', notes.length, 'notes') 
 
   const addNote = (event) => {
     event.preventDefault()
@@ -12,7 +23,7 @@ const App = (props) => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1
+      id: notes.length + 1,
     }
 
     setNotes(notes.concat(noteObject))
@@ -23,9 +34,7 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter((note) => note.important)
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   return (
     <div>
