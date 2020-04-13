b746709c4763f5e1aa134dae4cab27f43621a924
Mia Erbus <mia.erbus@gmail.com>
2020-04-13 14:29:33 +0200

    [part2] countries step1

---
+++ part2/countries/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Countries from './components/Countries'
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filteredName, setFilteredName] = useState('')
+
+  useEffect(() => {
+    axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
+      setCountries(response.data)
+    })
+  })
+
+  const handleFilterChange = (event) => {
+    setFilteredName(event.target.value)
+  }
+
+  const countriesFiltered =
+    filteredName !== ''
+      ? countries.filter(
+          (country) =>
+            country.name.toLowerCase().indexOf(filteredName.toLowerCase()) > -1
+        )
+      : []
+
+  return (
+    <div>
+      find countries
+      <input value={filteredName} onChange={handleFilterChange} />
+      <Countries countries={countriesFiltered} />
+    </div>
+  )
+}
+export default App
---
+++ part2/countries/src/components/Country.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Country = ({ country }) => {
+  const languages = country.languages.map((lang) => {
+    return <li key={lang.iso639_1}>{lang.name}</li>
+  })
+
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <div>capital {country.capital}</div>
+      <div>population {country.population}</div>
+      <h2>languages</h2>
+      <ul>{languages}</ul>
+      <img src={country.flag} alt={country.name} width="400" />
+    </div>
+  )
+}
+
+export default Country
