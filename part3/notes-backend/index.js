38efc48ba6e461d2edffe88af8a8c3187e10215e
Mia Erbus <mia.erbus@gmail.com>
2020-04-14 19:14:38 +0200

    [part3] notes middleware

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -3,6 +3,16 @@ const app = express()
 
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
 let notes = [
   {
     id: 1,
@@ -74,6 +84,12 @@ app.post('/api/notes', (request, response) => {
 
   response.json(note)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const port = 3001
 app.listen(port)
