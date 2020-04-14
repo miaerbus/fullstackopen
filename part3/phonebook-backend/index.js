a83f06f495d50d6d30ccdaede8505877d85204da
Mia Erbus <mia.erbus@gmail.com>
2020-04-14 17:27:22 +0200

    [part3] phonebook backend step5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -65,8 +65,7 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0
-  return maxId + 1
+  return Math.floor(Math.random() * Number.MAX_VALUE)
 }
 
 app.post('/api/persons', (request, response) => {
