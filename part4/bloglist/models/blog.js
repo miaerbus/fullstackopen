76cf72112d9c34863265d2ffc4a8dcd3bb665581
Mia Erbus <mia.erbus@gmail.com>
2020-04-16 17:32:21 +0200

    [part4] blog list step2

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -4,19 +4,6 @@ const mongoose = require('mongoose')
 mongoose.set('useFindAndModify', false)
 mongoose.set('useCreateIndex', true)
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose
-  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
