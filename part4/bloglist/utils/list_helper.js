4a8f1a53e97d4de160b74492e9c1444ce7cc043d
Mia Erbus <mia.erbus@gmail.com>
2020-04-16 18:26:01 +0200

    [part4] helper functions and unit tests step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => sum + item
+
+  return blogs.map((blog) => blog.likes).reduce(reducer, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
