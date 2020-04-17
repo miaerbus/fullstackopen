5925b5d3dd75b5d8fab989faa64176317446dc3e
Mia Erbus <mia.erbus@gmail.com>
2020-04-17 21:39:43 +0200

    [part4] blog list tests step2

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -6,7 +6,6 @@ const api = supertest(app)
 
 const Blog = require('../models/blog')
 
-
 beforeEach(async () => {
   await Blog.deleteMany({})
 
@@ -26,6 +25,12 @@ test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
   expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].id).toBeDefined()
 })
 
 afterAll(() => {
