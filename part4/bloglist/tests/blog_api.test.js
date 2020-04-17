c127e131f177ee040534cca767a7f884de756b63
Mia Erbus <mia.erbus@gmail.com>
2020-04-17 22:01:05 +0200

    [part4] blog list tests step4

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -55,6 +55,20 @@ test('a valid blog can be added', async () => {
   expect(titles).toContain('async/await simplifies making async calls')
 })
 
+test('blog without likes will default to 0', async () => {
+  const newBlog = {
+    title: 'default value of likes is 0',
+    author: 'Mia Erbus',
+    url: 'http://miaerbus.com',
+  }
+
+  await api.post('/api/blogs').send(newBlog).expect(201)
+
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[helper.initialBlogs.length].likes).toBe(0)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
