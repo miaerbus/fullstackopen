55f2a6e3efd627122953707f3d23b46490c2c9f8
Mia Erbus <mia.erbus@gmail.com>
2020-04-18 21:16:29 +0200

    [part4] blog list expansion step3

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const api = supertest(app)
@@ -21,5 +22,6 @@ mongoose
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
