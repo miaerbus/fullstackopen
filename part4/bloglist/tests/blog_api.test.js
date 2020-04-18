fd80abe5bd183542236b2b128ce62fb358e5bb02
Mia Erbus <mia.erbus@gmail.com>
2020-04-18 11:02:13 +0200

    [part4] blog list expansions

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -14,78 +14,100 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('unique identifier property of the blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body[0].id).toBeDefined()
+  })
 })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('adding a new blog', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'async/await simplifies making async calls',
+      author: 'Mia Erbus',
+      url: 'http://miaerbus.com',
+      likes: 10,
+    }
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map((r) => r.title)
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    expect(titles).toContain('async/await simplifies making async calls')
+  })
+
+  test('blog without likes will default to 0', async () => {
+    const newBlog = {
+      title: 'default value of likes is 0',
+      author: 'Mia Erbus',
+      url: 'http://miaerbus.com',
+    }
+
+    await api.post('/api/blogs').send(newBlog).expect(201)
+
+    const response = await api.get('/api/blogs')
+    expect(response.body[helper.initialBlogs.length].likes).toBe(0)
+  })
+
+  test('blog without title and url is not added', async () => {
+    const newBlog = {
+      author: 'Mia Erbus',
+    }
+
+    await api.post('/api/blogs').send(newBlog).expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
-test('unique identifier property of the blog posts is named id', async () => {
-  const response = await api.get('/api/blogs')
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-  expect(response.body[0].id).toBeDefined()
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+
+    const titles = blogsAtEnd.map((r) => r.title)
+    expect(titles).not.toContain(blogToDelete.title)
+  })
 })
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'async/await simplifies making async calls',
-    author: 'Mia Erbus',
-    url: 'http://miaerbus.com',
-    likes: 10,
-  }
+describe('updating a blog', () => {
+  test('succeeds with status code 200 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api.put(`/api/blogs/${blogToUpdate.id}`).expect(200)
 
-  const response = await api.get('/api/blogs')
-
-  const titles = response.body.map((r) => r.title)
-
-  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
-  expect(titles).toContain('async/await simplifies making async calls')
-})
-
-test('blog without likes will default to 0', async () => {
-  const newBlog = {
-    title: 'default value of likes is 0',
-    author: 'Mia Erbus',
-    url: 'http://miaerbus.com',
-  }
-
-  await api.post('/api/blogs').send(newBlog).expect(201)
-
-  const response = await api.get('/api/blogs')
-
-  expect(response.body[helper.initialBlogs.length].likes).toBe(0)
-})
-
-test('blog without title and url is not added', async () => {
-  const newBlog = {
-    author: 'Mia Erbus',
-  }
-    
-  const t = () => {
-    throw new ValidationError()
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
 afterAll(() => {
