de87552ad0522e990ce38fa155fae3741dd884eb
Mia Erbus <mia.erbus@gmail.com>
2020-04-19 15:46:30 +0200

    [part4] blog list expansion step8

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -8,14 +8,6 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs.map((b) => b.toJSON()))
 })
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
@@ -23,9 +15,8 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).end()
   }
 
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
   const user = await User.findById(decodedToken.id)
