5c75099a1d8675bb92160439eae73d616e683efe
Mia Erbus <mia.erbus@gmail.com>
2020-04-20 14:47:21 +0200

    [part5] blog list frontend step8

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
+import blogService from '../services/blogs'
 
 const Blog = ({ blog }) => {
   const [visible, setVisible] = useState(false)
+  const [updatedBlog, setUpdatedBlog] = useState(blog)
 
   const blogStyle = {
     paddingTop: 10,
@@ -11,21 +13,28 @@ const Blog = ({ blog }) => {
     marginBottom: 5,
   }
 
-  const handleClick = () => {
-    return setVisible(!visible)
+  const handleLikeChange = async () => {
+    const newObject = {
+      ...blog,
+      likes: blog.likes + 1,
+    }
+    const updatedBlog = await blogService.update(blog.id, newObject)
+    setUpdatedBlog(updatedBlog)
   }
 
   return (
     <div style={blogStyle}>
       <div>
         {blog.title} {blog.author}
-        <button onClick={handleClick}>{visible ? 'hide' : 'view'}</button>
+        <button onClick={() => setVisible(!visible)}>
+          {visible ? 'hide' : 'view'}
+        </button>
       </div>
       {visible && (
         <div>
           <div>{blog.url}</div>
           <div>
-            {blog.likes} <button>like</button>
+            {updatedBlog.likes} <button onClick={handleLikeChange}>like</button>
           </div>
           <div>{blog.user && blog.user.name}</div>
         </div>
