5a7aa0b4772977d0f4d4df2d6d4583754b261b3b
Mia Erbus <mia.erbus@gmail.com>
2020-04-20 21:58:56 +0200

    [part5] notes frontend eslint

--- part2/notes/src/components/LoginForm.js
+++ part2/notes/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = ({
   handleLogin,
@@ -29,5 +30,13 @@ const LoginForm = ({
     <button type="submit">login</button>
   </form>
 )
+
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+}
 
 export default LoginForm
