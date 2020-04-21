c0d9aa6f945b818f6b80e8c4ad08b9ba28000a72
Mia Erbus <mia.erbus@gmail.com>
2020-04-21 20:55:48 +0200

    [part5] blog list end to end testing step2

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -12,10 +12,31 @@ describe('Blog app', function () {
 
   it('Login from is shown', function () {
     cy.contains('log in to application')
-    cy.get('#username').type('root')
-    cy.get('#password').type('sekret')
-    cy.get('#login-button').click()
+    cy.contains('username')
+    cy.contains('password')
+    cy.contains('login')
+  })
 
-    cy.contains('Mia Erbus logged in')
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('root')
+      cy.get('#password').type('sekret')
+      cy.get('#login-button').click()
+
+      cy.contains('Mia Erbus logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('root')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'Wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', 'Mia Erbus logged in')
+    })
   })
 })
