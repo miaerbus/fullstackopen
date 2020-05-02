2a1837a39305d9de41a60cf0c1e7c06a006afb0f
Mia Erbus <mia.erbus@gmail.com>
2020-05-02 17:30:37 +0200

    [part8] graphql library frontend books view

--- part8/graphql-library-frontend/src/components/Books.js
+++ part8/graphql-library-frontend/src/components/Books.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+    }
+  }
+`
 
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
+
   if (!props.show) {
     return null
   }
 
-  const books = []
+  const books = result.data.allBooks
 
   return (
     <div>
@@ -15,20 +28,16 @@ const Books = (props) => {
         <tbody>
           <tr>
             <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
+            <th>author</th>
+            <th>published</th>
           </tr>
-          {books.map(a =>
+          {books.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
               <td>{a.published}</td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
     </div>
