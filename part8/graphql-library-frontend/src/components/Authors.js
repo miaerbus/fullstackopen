fdb1e7a9e312a7e1b15ff15a6976ee96f565f37a
Mia Erbus <mia.erbus@gmail.com>
2020-05-03 11:28:09 +0200

    [part8] graphql library frontend authors birth year

--- part8/graphql-library-frontend/src/components/Authors.js
+++ part8/graphql-library-frontend/src/components/Authors.js
@@ -1,9 +1,14 @@
-import React from 'react'
-import { useQuery } from '@apollo/client'
-import { ALL_AUTHORS } from '../queries'
+import React, { useState } from 'react'
+import { useQuery, useMutation } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
 
 const Authors = (props) => {
   const result = useQuery(ALL_AUTHORS)
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  })
 
   if (!props.show) {
     return null
@@ -14,6 +19,15 @@ const Authors = (props) => {
   }
 
   const authors = result.data.allAuthors
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    editAuthor({ variables: { name, setBornTo: born } })
+
+    setName('')
+    setBorn('')
+  }
 
   return (
     <div>
@@ -34,6 +48,25 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <h2>set birthyear</h2>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            type="number"
+            value={born}
+            onChange={({ target }) => setBorn(Number(target.value))}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   )
 }
